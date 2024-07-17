a9d8534d64a5abf15d2b633684084fb9c46c0bb4
kdandawate1991 <kdandawate1991@gmail.com>
2024-07-17 11:43:14 +1000

    Update crud.py

--- api_testing/api/crud.py
+++ api_testing/api/crud.py
@@ -12,24 +12,36 @@ class Crud:
     def get_resource(self, endpoint, params=None):
         url = self.base_url + endpoint
         response = requests.get(url, params=params, verify=False)
-        logger.debug(f'endpoint = {endpoint}\nresponse = {response.json()}')
+        resp_data = None
+        if response.status_code == 200:
+            resp_data = response.json()
+        logger.debug(f'endpoint = {endpoint}\nresponse = {resp_data}')
         return response
 
     def create_resource(self, endpoint, payload=None, files=None):
         url = self.base_url + endpoint
         response = requests.post(url, json=payload, files=files, verify=False)
-        logger.debug(f'endpoint = {endpoint}\nresponse = {response.json()}')
+        resp_data = None
+        if response.status_code == 200:
+            resp_data = response.json()
+        logger.debug(f'endpoint = {endpoint}\nresponse = {resp_data}')
         return response
 
     def update_resource(self, endpoint, payload=None):
         url = self.base_url + endpoint
         response = requests.patch(url, json=payload, verify=False)
-        logger.debug(f'endpoint = {endpoint}\nresponse = {response.json()}')
+        resp_data = None
+        if response.status_code == 200:
+            resp_data = response.json()
+        logger.debug(f'endpoint = {endpoint}\nresponse = {resp_data}')
         return response
 
     def delete_resource(self, endpoint):
         url = self.base_url + endpoint
         response = requests.delete(url, verify=False)
-        logger.debug(f'endpoint = {endpoint}\nresponse = {response.json()}')
+        resp_data = None
+        if response.status_code == 200:
+            resp_data = response.json()
+        logger.debug(f'endpoint = {endpoint}\nresponse = {resp_data}')
         return response
 
